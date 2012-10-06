154df0bdea932bb16a8e6496a501d1e7ee9e4677
wangjunhui <1010888134@qq.com>
2012-10-06 11:08:03 +0800

    添加接口文件

--- eoesoso/src/com/eoesou/service/SosoService.java
+++ eoesoso/src/com/eoesou/service/SosoService.java
@@ -3,30 +3,35 @@ package com.eoesou.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class SosoService extends Service{
-	
+	public static final String TAG = ">>>SosoService<<<";
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
+		Log.i(TAG, "------> onCreate()");
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
+		Log.i(TAG, "------> onBind()");
 		return null;
 	}
 	
 	@Override
 	public boolean onUnbind(Intent intent) {
 		// TODO Auto-generated method stub
+		Log.i(TAG, "------> onUnbind()");
 		return super.onUnbind(intent);
 	}
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		// TODO Auto-generated method stub
+		Log.i(TAG, "------> onStartCommand()");
 		return super.onStartCommand(intent, flags, startId);
 	}
 	
@@ -34,6 +39,7 @@ public class SosoService extends Service{
 	public void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
+		Log.i(TAG, "------> onDestroy()");
 	}
 
 }
